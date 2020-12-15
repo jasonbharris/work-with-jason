50f19c8b1165b567dbaae3e22ceb4d4e5757649f
System Administrator <root@Jasons-MacBook-Pro.local>
2020-12-14 19:40:27 -0600

    got data coming in from Sanity and now need to work on styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import sanityClient from '../client'
 
 const Home = () => {
+  const [allPostsData, setAllPosts] = useState(null)
+
+  useEffect(() => {
+    sanityClient.fetch(
+      `*[_type == 'post']{
+        title,
+        slug,
+        mainImage{
+          asset->{
+            _id,
+            url
+          }
+        }
+      }`
+    )
+    .then((data) => setAllPosts(data))
+    .catch(console.error)
+  }, [])
+
   return (
     <div>
-      <p>This is the Home page</p>
+      <h2>Work With Jason</h2>
+      <h3>Welcome to my blog</h3>
+      <div>
+      {allPostsData &&
+        allPostsData.map((post, index) => (
+          <Link to={'/' + post.slug.current} key={post.slug.current}>
+            <span key={index}>
+              <img src={post.mainImage.asset.url} alt='Main hero image for react blog' />
+              <span>
+                <h2>{post.title}</h2>
+              </span>
+            </span>
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
--- src/pages/SinglePostComponent.js
+++ src/pages/SinglePostComponent.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import sanityClient from '../client'
+import imageUrlBuilder from '@sanity/image-url'
+import BlockContent from '@sanity/block-content-to-react'
+
+const builder = imageUrlBuilder(sanityClient)
+const urlFor = (source) => {
+  return builder.image(source)
+}
+
+
 
 const SinglePostComponent = () => {
+  const [postData, setPostData] = useState(null)
+  const { slug } = useParams()
+
+  useEffect(() => {
+    sanityClient.fetch(
+      `*[slug.current == $slug]{
+        title,
+        slug,
+        mainImage{
+          asset->{
+            _id,
+            url
+          }
+        },
+        body,
+        "name": author->name,
+        "authorImage": author->image
+      }`,
+      { slug }
+    )
+    .then((data) => setPostData(data[0]))
+    .catch(console.error)
+  }, [slug])
+
+  if(!postData) return <div>Loading...</div>
+
   return (
     <div>
-      <p>This is the Single Post Component page</p>
+      <div>
+        <h2>{postData.title}</h2>
+        <div>
+          <img src={urlFor(postData.authorImage).width(100).url()} alt="Author is Jason Harris" />
+          <h4>{postData.name}</h4>
+        </div>
+      </div>
+      <img src={urlFor(postData.mainImage).width(200).url()} alt="Main Post Image" />
+      <div>
+        <BlockContent
+          blocks={postData.body}
+          projectId={sanityClient.clientConfig.projectId}
+          dataset={sanityClient.clientConfig.dataset}
+        />
+      </div>
     </div>
   )
 }
